364c9aed6ee648334bea3f2d835a425c34f9ff9e
Richard Sanders <rsanders@ulterior.io>
2019-02-11 22:05:10 -0500

    added posh and updated stack.yml
    Signed-off-by: Richard Sanders <rsanders@ulterior.io>

--- run-posh/handler.ps1
+++ run-posh/handler.ps1
@@ -1,13 +1,25 @@
 function Handler {
-  Param(
-  [Parameter(Mandatory=$true)]
-  [FunctionContext]$fnContext,
-  [Parameter(Mandatory=$true)]
-  [FunctionResponse]$fnResponse
-)
-
-$output = "Hello! Your input was: " + $fnContext.Body
-
-$fnResponse.Body = $output
+    Param(
+        [Parameter(Mandatory = $true)]
+        [FunctionContext]$fnContext,
+        [Parameter(Mandatory = $true)]
+        [FunctionResponse]$fnResponse
+    )
   
+    $data = $fnContext.Body
+    if ([string]::IsNullOrEmpty($data)) {
+        $out = $(Get-Module -ListAvailable |
+                ForEach-Object {
+                  'Module Name:{0}' -f $_
+                Get-Command -Module $_.name -CommandType cmdlet, function |
+                    ForEach-Object {
+                      "`t{0}" -f $_.Name
+                }
+            })
+        $fnResponse.Body = $out
+    }
+    else {
+        $out = $(pwsh -c $data)
+        $fnResponse.Body = $out
+    } 
 }
